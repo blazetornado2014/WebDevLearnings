99f2c1cb6272f366219dcac2225863ec41c12f9d
blazetornado2014 <155018915+blazetornado2014@users.noreply.github.com>
2025-01-14 19:25:28 -0700

    Update index.js
    Added sound on button press

--- Testing/housekeeper.js
+++ Testing/housekeeper.js
@@ -2,6 +2,9 @@ function HouseKeeper(yearsOfExp, name, cleaningRepertoire){
     this.yearsOfExp = yearsOfExp;
     this.name = name;
     this.cleaningRepertoire = cleaningRepertoire;
+    this.clean = function () {
+        alert("cleaning in progress");
+    }
 }
 
 var houseKeeper1 = new HouseKeeper(12, 'Angela', ['bathroom', 'kitchen']);
